d271aedc72944fb947d1510dc7a3a4f50fb178f4
Jon Ander Gómez <jon@dsic.upv.es>
2021-07-04 13:29:17 +0200

    Changes while testing

---
+++ issue_294/scripts/run-fc-wings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib"
+#export LD_PRELOAD="${CONDA_PREFIX}/lib/libasan.so"
+
+build/fully_conn_wings.exe $*
+#valgrind --leak-check=full --track-origins=yes build/fully_conn_wings.exe --reduced $*
